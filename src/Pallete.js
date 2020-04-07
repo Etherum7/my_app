91011e13a6f0850d40879f51fec2a4aa91549d9d
Vardhan7293 <hvs.231917@gmail.com>
2020-04-07 22:55:38 +0530

    Added chroma-js and logic

--- src/Pallete.js
+++ src/Pallete.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox';
+import {generatePallete} from './ColorHelpers';
+import SeedColors from './SeedColors'
 import './Pallete.css';
 class Pallete extends Component {
     render() {
+        console.log(generatePallete(SeedColors[4]));
     const colorBoxes = this.props.colors.map(color => (
     <ColorBox background ={color.color} name={color.name} />
     )); 
         return (
+            
             <div className="Pallete">
                 {/* NavBar goes here */}
                 <div className='Pallete-colors'>
